5f396797c98dc86f7d7b71b2862d84ae71975a52
Diego Ribeiro <dmrbeiro87@gmail.com>
2020-09-26 23:17:10 -0300

    Very bad solution

---
+++ src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import model.entities.Reservation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) throws ParseException {
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+        System.out.print("Room number: ");
+        int roomNumber = scanner.nextInt();
+        System.out.print("Check-in date: ");
+        Date checkIn = simpleDateFormat.parse(scanner.next());
+        System.out.print("Check-out date: ");
+        Date checkOut = simpleDateFormat.parse(scanner.next());
+
+        if (!checkOut.after(checkIn)){
+            System.out.println("Error in reservation: check-out must be after check-in date!");
+        }else {
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+            System.out.println("Reservation: " + reservation);
+
+            System.out.println();
+            System.out.println("enter data to update reservation: ");
+            System.out.print("Check-in date: ");
+            checkIn = simpleDateFormat.parse(scanner.next());
+            System.out.print("Check-out date: ");
+            checkOut = simpleDateFormat.parse(scanner. next());
+
+            Date now = new Date();
+
+            if (checkIn.before(now) || checkOut.before(now)){
+                System.out.println("Error in reservation: reservation date must be future!");
+            }else if (!checkIn.after(checkIn)){
+                System.out.println("Error in reservation: check-out must be after check-in date!");
+            }else {
+                reservation.updateDates(checkIn, checkOut);
+                System.out.println("Reservation: " + reservation);
+            }
+
+
+        }
+
+
+        scanner.close();
+    }
+}
